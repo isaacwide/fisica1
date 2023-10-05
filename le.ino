4c6851c3c6ecd9d791da07291ad977d7c76b6d69
isaacwide <145953724+isaacwide@users.noreply.github.com>
2023-10-05 12:14:48 -0600

    Create le.ino
    hola

---
+++ le.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+//hola camaradas
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);   
+  // segundo foquito
+  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
+}
